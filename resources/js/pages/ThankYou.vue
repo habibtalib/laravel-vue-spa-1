932b9acb2d1acc5530ce909a791d5df8da28e8b4
Habib Talib <habib.tech4life@gmail.com>
2019-03-29 12:37:19 +0800

    adjust image and link

--- resources/js/pages/ThankYou.vue
+++ resources/js/pages/ThankYou.vue
@@ -3,8 +3,14 @@
     <v-container class="text-xs-center">
       <h3>Thank You for Your Registration</h3>
       <p>Please Download Our App</p>
-      <v-img src="/images/googleplay.png" lazy-src="/images/googleplay.png" aspect-ratio="3.10"></v-img>
-      <v-img src="/images/applestore.png" lazy-src="/images/applestore.png" aspect-ratio="3.8"></v-img>
+      <v-img
+        src="/images/googleplay.png"
+        @click="googlePlay"
+        lazy-src="/images/googleplay.png"
+        aspect-ratio="3.10"
+      ></v-img>
+      <v-spacer></v-spacer>
+      <v-img src="/images/applestore.png" lazy-src="/images/applestore.png" aspect-ratio="3.9"></v-img>
     </v-container>
   </v-content>
 </template>
@@ -13,6 +19,10 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  methods: {
+    googlePlay() {
+      window.open("/meeracle.apk");
+    }
+  }
 };
 </script>
