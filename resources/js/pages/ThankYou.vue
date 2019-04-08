b4c959cce8647c67108c9a980c12d7351f6050c1
Habib Talib <habib.tech4life@gmail.com>
2019-04-09 03:07:02 +0800

    Comment google play

--- resources/js/pages/ThankYou.vue
+++ resources/js/pages/ThankYou.vue
@@ -2,16 +2,16 @@
   <v-content>
     <v-container class="text-xs-center">
       <h3>Thank You for Your Registration</h3>
-      <p>Please Download Our App</p>
-      <v-img
+      <!-- <p>Please Download Our App</p> -->
+      <!-- <v-img
         position="center"
         src="/images/googleplay.png"
         @click="googlePlay"
         lazy-src="/images/googleplay.png"
         max-width="600"
-      ></v-img>
-      <v-spacer></v-spacer>
-      <v-img src="/images/applestore.png" lazy-src="/images/applestore.png" max-width="600"></v-img>
+      ></v-img>-->
+      <!-- <v-spacer></v-spacer>
+      <v-img src="/images/applestore.png" lazy-src="/images/applestore.png" max-width="600"></v-img>-->
     </v-container>
   </v-content>
 </template>
